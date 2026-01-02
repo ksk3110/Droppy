550ca94f99b633fe3b4df6fc4b8ceec4e71ae8fe
iordv <jordy.spruit@newyorkpizza.nl>
2026-01-02 21:56:40 +0100

    Release v1.2

--- Casks/droppy.rb
+++ Casks/droppy.rb
@@ -1,6 +1,6 @@
 cask "droppy" do
   version "1.2"
-  sha256 "09c62ef8d5ec575fdd9c612df6295ac477dbd745253fab3d3758ff23763c8d60"
+  sha256 "6b99d7f5ee6c9aaa7b6d5f86f6e3d86813f3ce9f66b03fb52a180f9d893a1c97"
 
   url "https://raw.githubusercontent.com/iordv/Droppy/main/Droppy.dmg"
   name "Droppy"
