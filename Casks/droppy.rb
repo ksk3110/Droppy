efc8a6ced402e5328b1ae38e7753dd6c03a7d4c4
iordv <jordy.spruit@newyorkpizza.nl>
2026-01-14 17:14:54 +0100

    Release v7.7.1

--- Casks/droppy.rb
+++ Casks/droppy.rb
@@ -1,8 +1,8 @@
 cask "droppy" do
-  version "7.7"
-  sha256 "404575847e719e295d0589f2f2c758f13f47420e4845d09c57af12653e54a19b"
+  version "7.7.1"
+  sha256 "0376d0a983bb5de75384287c56268abbe9b1ef9c0fc64a671cc7190373ce6579"
 
-  url "https://github.com/iordv/Droppy/releases/download/v7.7/Droppy-7.7.dmg"
+  url "https://github.com/iordv/Droppy/releases/download/v7.7.1/Droppy-7.7.1.dmg"
   name "Droppy"
   desc "Drag and drop file shelf for macOS"
   homepage "https://github.com/iordv/Droppy"
